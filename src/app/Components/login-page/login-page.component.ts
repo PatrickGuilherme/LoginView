77f213ffa26f11670464f0353cbe2f8a728d75f4
Patrick Guilherme da Silva <patrick.guilhermedasilva@gmail.com>
2022-08-02 12:26:13 -0300

    Criação de design e validações da tela de login

--- src/app/Components/login-page/login-page.component.ts
+++ src/app/Components/login-page/login-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login-page',
@@ -6,10 +7,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login-page.component.css']
 })
 export class LoginPageComponent implements OnInit {
+  public LoginForm:FormGroup;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
   }
 
+  ngOnInit(): void {
+    this.LoginForm = new FormGroup({
+      "email": new FormControl(null, [Validators.required, Validators.email]),
+      "password": new FormControl(null, [Validators.required])
+    });
+  }
+  
+  //Validação do e-mail
+  public EmailInvalid():string {
+    if(this.LoginForm.getError("required","email") && this.LoginForm.get("email").touched) {
+      return "Preencha o e-mail";
+    }else if(this.LoginForm.getError("email","email") && this.LoginForm.get("email").touched) {
+      return "E-mail inválido, tente novamente";
+    }else {
+      var regex = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
+      if(!regex.test(this.LoginForm.value.email) && this.LoginForm.get("email").touched) {
+        return "E-mail inválido, tente novamente";
+      }
+    }
+    return null;
+  }
+
+  //Validação da senha
+  public PasswordInvalid():string {
+    if(this.LoginForm.getError("required","password") && this.LoginForm.get("password").touched) {
+      return "Preencha a senha";
+    }
+    return null;
+  }
+
+  public Login():void {}
 }
